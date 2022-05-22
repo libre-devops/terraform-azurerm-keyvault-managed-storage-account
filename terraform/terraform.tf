6fd479b6b554090691f942b9dd8d794031a0d14e
Craig Thacker <craigthackerx@gmail.com>
2022-05-22 20:26:13 +0100

    Update

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.0.9"
+  required_version = ">= 1.1.0, < 1.2.0"
   #Use the latest by default, uncomment below to pin or use hcl.lck
   required_providers {
     azurerm = {
