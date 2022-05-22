4164390d956e064aabc8bf9ab5f6fa23f64e6a2f
Craig Thacker <craigthackerx@gmail.com>
2022-05-22 21:33:47 +0100

    Update

--- iam.tf
+++ iam.tf
@@ -1,7 +1,7 @@
 resource "azurerm_role_assignment" "sa_operator_role_assignment" {
   scope                = azurerm_storage_account.sa.id
   role_definition_name = "Storage Account Key Operator Service Role"
-  principal_id         = "727055f9-0386-4ccb-bcf1-9237237ee102"
+  principal_id         = "2f52b87a-b032-421f-b0fb-3c8be030d053" // Azure Key Vault service role in tenant
 
   timeouts {
     create = "5m"
