9a286777cba4298e40103b64fc6b2859b9759b88
Craig Thacker <craigthackerx@gmail.com>
2022-05-22 21:24:22 +0100

    Update

--- iam.tf
+++ iam.tf
@@ -1,7 +1,7 @@
 resource "azurerm_role_assignment" "sa_operator_role_assignment" {
   scope                = azurerm_storage_account.sa.id
   role_definition_name = "Storage Account Key Operator Service Role"
-  principal_id         = "cfa8b339-82a2-471a-a3c9-0fc0be7a4093"
+  principal_id         = "727055f9-0386-4ccb-bcf1-9237237ee102"
 
   timeouts {
     create = "5m"
